63344c6e10e69430faef3c678353083387a67ea2
Debasis Nayak <debasishnayak0739@gmail.com>
2024-02-26 10:36:51 +0530

    done

---
+++ src/components/Parent.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Child from "./Child";
+
+const Parent = () => {
+    const [todos,setTodos] = useState([
+        {id:1, text:"Learn React", completed:false},
+        {id:2, text:"Build a React app", completed:false},
+        {id:3, text:"Deploy the React app", completed:false}
+    ]);
+
+    function handleComplete(id){
+        setTodos(todos.map(todo => {
+            if(todo.id === id){
+                return {...todos,completed: true};
+            }
+            return todo;
+        }));
+    }
+
+    return(
+        <div>
+            <h1>Parent Component</h1>
+            <Child todos={todos} handleComplete={handleComplete}/>
+        </div>
+    )
+}
+export default Parent;
