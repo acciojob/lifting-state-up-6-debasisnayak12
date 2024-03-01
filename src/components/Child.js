88eb8a481b9c88792adb800b254fa1296524098c
Debasis Nayak <debasishnayak0739@gmail.com>
2024-03-01 09:30:27 +0530

    re

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,22 +1,51 @@
+// import React from "react";
+
+// const Child = ({todos , handleComplete}) => {
+
+//     return (
+//       <div className="child">
+//         <h2>Child Component</h2>
+//         <ul>
+//           {todos.map((todo) => (
+//               <li key={todo.id}>
+//                {todo.text}
+//                 { !todo.completed && (
+//                 <button onClick={()=>{handleComplete(todo.id)}}>Complete</button>
+//                 )}
+//               </li>
+//           ))}
+//         </ul>
+//       </div>
+//     );
+// }
+
+// export default Child;
+
+
 import React from "react";
 
-const Child = ({todos , handleComplete}) => {
-
-    return (
-      <div>
-        <h2>Child Component</h2>
-        <ul>
-          {todos.map((todo) => (
-              <li key={todo.id}>
-               {todo.text}
-                { !todo.completed && (
-                <button onClick={()=>{handleComplete(todo.id)}}>Complete</button>
-                )}
-              </li>
-          ))}
-        </ul>
-      </div>
+const Child = ({ taskList, setTaskList }) => {
+  function done(key) {
+    setTaskList((taskList) =>
+      taskList.filter((task) => {
+        if (task.key === key) task.status = true;
+        return task;
+      })
     );
-}
+  }
+  return (
+    <div className="child">
+      <h2>Child Component</h2>
+      {taskList.map((taskTodo) => (
+        <li key={taskTodo.key}>
+          {taskTodo.task}
+          {!taskTodo.status && (
+            <button onClick={() => done(taskTodo.key)}>Complete</button>
+          )}
+        </li>
+      ))}
+    </div>
+  );
+};
 
 export default Child;
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,27 +1,47 @@
+// import React, { useState } from "react";
+// import Child from "./Child";
+
+// const Parent = () => {
+//     const [todos,setTodos] = useState([
+//         {id:1, text:"Learn React", completed:false},
+//         {id:2, text:"Build a React app", completed:false},
+//         {id:3, text:"Deploy the React app", completed:false}
+//     ]);
+
+//     function handleComplete(id){
+//         setTodos(todos.map(todo => {
+//             if(todo.id === id){
+//                 return {...todos,completed: true};
+//             }
+//             return todo;
+//         }));
+//     }
+
+//     return(
+//         <div className="parent">
+//             <h1>Parent Component</h1>
+//             <Child todos={todos} handleComplete={handleComplete}/>
+//         </div>
+//     )
+// }
+// export default Parent;
+
+
 import React, { useState } from "react";
 import Child from "./Child";
-
 const Parent = () => {
-    const [todos,setTodos] = useState([
-        {id:1, text:"Learn React", completed:false},
-        {id:2, text:"Build a React app", completed:false},
-        {id:3, text:"Deploy the React app", completed:false}
-    ]);
+  const [taskList, setTaskList] = useState([
+    { key: 0, task: "Learn React", status: false },
+    { key: 1, task: "Build a React app", status: false },
+    { key: 2, task: "Deploy the React app", status: false },
+  ]);
 
-    function handleComplete(id){
-        setTodos(todos.map(todo => {
-            if(todo.id === id){
-                return {...todos,completed: true};
-            }
-            return todo;
-        }));
-    }
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <Child taskList={taskList} setTaskList={setTaskList} />
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <h1>Parent Component</h1>
-            <Child todos={todos} handleComplete={handleComplete}/>
-        </div>
-    )
-}
 export default Parent;
